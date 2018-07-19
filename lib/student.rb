751b0ba409499267f3fea589bb7b14f55c94b389
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-19 20:49:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,9 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE grade = "10" LIMIT 1
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
